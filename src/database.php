4ea09c825a819167c2d7ad1eff6a280cf33f6ae5
Loogman <koen.loogman@atos.net>
2018-02-27 12:57:56 +0100

    Added comments and  documentation

--- src/database.php
+++ src/database.php
@@ -1,8 +1,17 @@
 <?php
+// include config
 include_once("config.php");
 
+/**
+ * Opens a new mysqli connection with the in the config.php defined login information.
+ * 
+ * @return mysqli mysqli object with the connection to the database.
+ */
 function new_msqli() {
+    // get config data
     $config = $GLOBALS["_CONFIG"];
+
+    // return the mysqli object
     return new mysqli($config["sql_uri"], $config["sql_username"], $config["sql_password"], $config["sql_database"]);
 }
 ?>
