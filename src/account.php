e35d1c9a081701096d2e2819ad87b2afb1df57af
Loogman <koen.loogman@atos.net>
2018-02-27 12:56:32 +0100

    updated comments and added more documentation

--- src/account.php
+++ src/account.php
@@ -1,10 +1,16 @@
 <?php
+// start session if not already started
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
+// include database functions
 include_once("database.php");
 
+/**
+ * Logs out the current user.
+ * @return boolean true if successfully logged out false if not
+ */
 function logout() {
     if (is_loggedin()) {
         unset($_SESSION["username"]);
@@ -14,19 +20,27 @@ function logout() {
     }
 }
 
-function login($username, $password) {
-    $msqli = new_msqli();
+/**
+ * Logs in a user.
+ * 
+ * @param string $username of the user.
+ * @param string $password of the user.
+ * 
+ * @return boolean true if login was successful and false if not.
+ */
+function login(string $username, string $password) {
+    $msqli = new_mysqli();
     
-    # Check connection
+    // Check connection
     if ($msqli->connect_errno) {
         echo "Failed to connect to MySQL: ".$msqli->connect_error;
     }
 
-    # prepare statement to obtain the hashed password
+    // prepare statement to obtain the hashed password
     $stmt = $msqli->prepare("SELECT `password` FROM `user` WHERE `username` LIKE ?;");
     $stmt->bind_param("s", $username);
 
-    # get hashed password
+    // get hashed password
     $stmt->execute();
     $res = $stmt->get_result();
     $hashed_password = null;
@@ -34,13 +48,13 @@ function login($username, $password) {
         $hashed_password = $row["password"];
     }
 
-    # close db connection
+    // close db connection
     $stmt->close();
     $res->close();
     $msqli->close();
     
     if ($hashed_password != null && password_verify($password, $hashed_password)) {
-        # set username to session
+        // set username to session
         $_SESSION["username"] = $username;
         
         return true;
@@ -49,15 +63,20 @@ function login($username, $password) {
     }
 }
 
+/**
+ * Checks if the username is in use or not.
+ * 
+ * @return boolean true if username is already in use and false if not
+ */
 function username_taken($username) {
     $msqli = new_msqli();
 
-    # Check connection
+    // Check connection
     if ($msqli->connect_errno) {
         echo "Failed to connect to MySQL: ".$msqli->connect_error;
     }
 
-    # prepare statement to check for the username
+    // prepare statement to check for the username
     $stmt = $msqli->prepare("SELECT `id` FROM `user` WHERE `username` LIKE ?;");
     $stmt->bind_param("s", $username);
 
@@ -68,43 +87,59 @@ function username_taken($username) {
         $id = $row["id"];
     }
 
-    # close db connection
+    // close db connection
     $stmt->close();
     $res->close();
     $msqli->close();
 
-    # return true if found and false if not
+    // return true if found and false if not
     return ($id != null);
 }
 
-function new_user($username, $password) {
+/**
+ * Creates a new user.
+ * 
+ * @param string $username the name of the new user.
+ * @param string $password the password for the new user.
+ * 
+ * @return boolean true if the creation was successful and false if not.
+ */
+function new_user(string $username, string $password) {
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     $msqli = new_msqli();
 
-    # Check connection
+    // Check connection
     if ($msqli->connect_errno) {
         echo "Failed to connect to MySQL: ".$msqli->connect_error;
     }
 
-    # prepare statement to check for the username
+    // prepare statement to check for the username
     $stmt = $msqli->prepare("INSERT INTO `user`(`username`, `password`) VALUES (?, ?);");
     $stmt->bind_param("s", $username);
     $stmt->bind_param("s", $hashed_password);
     
     $success = $stmt->execute();
     
-    # close db connection
+    // close db connection
     $stmt->close();
     $msqli->close();
 
     return $success;
 }
 
+/**
+ * Checks if a user is currently logged in.
+ * 
+ * @return boolean true if the user is logged in and false if not.
+ */
 function is_loggedin() {
     return (isset($_SESSION["username"]) && $_SESSION["username"] != "" && $_SESSION["username"] != null);
 }
 
+/**
+ * @return string name of the current user and null if no user is logged in.
+ */
 function username() {
     return isset($_SESSION["username"]) ? $_SESSION["username"] : null;
 }
