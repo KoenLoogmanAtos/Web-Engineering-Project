36b36e5e64e0832d1d753c7abe0d460f50dcf350
Loogman <koen.loogman@atos.net>
2018-02-27 11:15:15 +0100

    did a few tests

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+include_once("src/config.php");
+include_once("src/account.php");
+
+if (isset($_POST["username"], $_POST["password"]) && !is_loggedin()) {
+    login($_POST["username"], $_POST["password"]);
+}
+
+if (isset($_POST["action"]) && $_POST["action"] == "logout") {
+    logout();
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,7 +22,29 @@
     </head>
     <body>
         <?php
-            echo "Hello World.";
+            if (!is_loggedin()) {
+        ?>
+        <form action="" method="post">
+            <input type="text" name="username"><br>
+            <input type="password" name="password"><br>
+            <input type="submit" value="Login">
+        </form>
+        <?php
+            } else {
+        ?>
+        <form action="" method="post">
+            <input type="hidden" name="action" value="logout">
+            <input type="submit" value="Logout">
+        </form>
+        <?php
+                echo "Hello ".username()."!";
+            }
+
+            if (username_taken("Admin")) {
+                echo "Yes";
+            } else {
+                echo "No";
+            }
         ?>
     </body>
 </html>
