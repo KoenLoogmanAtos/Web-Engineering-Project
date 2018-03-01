d87434bb1252c08cafd96a29e27a5b51a2ababdb
Loogman <koen.loogman@atos.net>
2018-03-01 13:24:52 +0100

    more testing

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
 include_once("src/config.php");
-include_once("src/database.php");
 include_once("src/account.php");
 
 // login the user if login form was send and no user is currently logged in
-if (isset($_POST["username"], $_POST["password"]) && !is_loggedin()) {
+if (isset($_POST["username"], $_POST["password"], $_POST["action"]) && !is_loggedin() && strtolower($_POST["action"]) == "login") {
     login($_POST["username"], $_POST["password"]);
 }
 
 // logout if the action was send
 if (isset($_POST["action"]) && strtolower($_POST["action"]) == "logout") {
     logout();
+}
+
+if (isset($_POST["username"], $_POST["password"], $_POST["action"]) && strtolower($_POST["action"]) == "new_user") {
+    new_user($_POST["username"], $_POST["password"]);
 }
 ?>
 <!DOCTYPE html>
@@ -31,11 +34,12 @@ if (isset($_POST["action"]) && strtolower($_POST["action"]) == "logout") {
 
                 if (is_loggedin()) {
                     ?><form id="logout" action="" method="post" ajax="true">
-                        <input type="hidden" value="Logout" name="action"/>
+                        <input type="hidden" name="action" value="logout"/>
                         <input type="submit" value="Logout"/>
                     </form><?php
                 } else {
                     ?><form id="login" action="" method="post" ajax="true">
+                    <input type="hidden" name="action" value="login"/>
                         <input type="text" placeholder="username" name="username"/>
                         <input type="password" placeholder="password" name="password"/>
                         <input type="submit" value="Login"/>
@@ -48,15 +52,52 @@ if (isset($_POST["action"]) && strtolower($_POST["action"]) == "logout") {
         <div id="content">
             <div class="wrapper">
                 <?php
-                    if (is_loggedin()) {
-                        echo "Hello ".username()."!";
+                    $mysqli = new_mysqli();
+
+                    $stmt = $mysqli->prepare("SELECT u.`id`,  u.`username`, r.`role` FROM `user` as u, `user_roles` as r WHERE u.`user_role_id` = r.`id`;");
+                    $stmt->execute();
+                    $res = $stmt->get_result();
+                    
+                    echo "<table>"
+                        ."<tr><th>Username</th><th>Role</th></tr>";
+                    while ($row = $res->fetch_assoc()) {
+                        echo "<tr><td>".$row["username"]."</td><td>".$row["role"]."</td></tr>";
                     }
+                    echo "</table>";
+
+                    $res->close();
+                    $stmt->close();
+
+                    if (is_loggedin()) {
+                        ?>
+                        <form action="" method="post" ajax="true">
+                            <input type="hidden" name="action" value="new_user"/>
+                            <input type="text" name="username" placeholder="username"/>
+                            <input type="password" name="password" placeholder="password"/>
+                            <input type="submit" value="New User"/>
+                        </form>
+                        <?php
+                    }
+
+                    $mysqli->close();
                 ?>
             </div>
         </div>
         <div id="footer">
             <div class="wrapper">
-                Footer
+                <?php
+                    if (is_loggedin()) {
+                        echo "<pre>GET\n";
+                            print_r($_GET);
+                        echo "</pre>";
+                        echo "<pre>POST\n";
+                            print_r($_POST);
+                        echo "</pre>";
+                        echo "<pre>SESSION\n";
+                            print_r($_SESSION);
+                        echo "</pre>";
+                    }
+                ?>
             </div>
         </div>
     </body>
