90af205a970b5a6eca0bd5ea447fa398523382a1
Kader <aleyna.kader@atos.net>
2018-03-19 18:05:28 +0100

    Implemented functions

--- src/Controller/ApiGuestController.php
+++ src/Controller/ApiGuestController.php
@@ -5,15 +5,118 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ApiGuestController extends Controller
+class ApiGuestController extends JmsController
 {
     /**
-     * @Route("/api/guest", name="api_guest")
+     * @Route(methods={"GET", "HEAD"}, name="api_guest_index")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('api_guest/index.html.twig', [
-            'controller_name' => 'ApiGuestController',
-        ]);
+        $data = array();
+
+        $guests = array();
+        $guests = $this->getDoctrine()->getRepository(Guest::class)->findAll();
+        $data["reports"] = $guests;
+
+        return $this->jms_json($data);
+    }
+
+    /**
+     * @Route("/{id}", methods={"GET"}, requirements={"id"="\d{1,10}"}, name="api_guest_show")
+     */
+    public function show($id)
+    {
+        $data = array();
+
+        $guest = $this->getDoctrine()->getRepository(Guest::class)->find($id);
+        $data["reports"] = array($guest);
+
+        return $this->jms_json($data);
+    }
+
+    /**
+     * @Route(methods={"POST"}, name="api_guest_create")
+     */
+    public function create(Request $request)
+    {
+        $data = array();
+
+        $guest = new guest();
+        $form = $this->createForm(guestType::class, $guest);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $guest = $form->getData();
+
+            try {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($guest);
+                $em->flush();
+                $data["message"] = "Successfully created";
+            } catch(\Exception $e) {
+                $data["message"] = "Failed to create";
+
+                //TODO cleaner error handling
+                $data["error"] = $e->getMessage();
+            }
+        }
+
+        return $this->jms_json($data);
+    }
+
+    /**
+     * @Route("/{id}", methods={"PUT"}, requirements={"id"="\d{1,10}"}, name="api_guest_edit")
+     */
+    public function edit($id, Request $request)
+    {
+        $data = array();
+
+        $guest = $this->getDoctrine()->getRepository(Guest::class)->find($id);
+        $form = $this->createForm(guestType::class, $guest);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $guest = $form->getData();
+            
+            try {
+                $em = $this->getDoctrine()->getManager();
+                $em->merge($guest);
+                $em->flush();
+                $data["message"] = "Update was successful";
+            } catch(\Exception $e) {
+                $data["message"] = "Update failed";
+
+                //TODO cleaner error handling
+                $data["error"] = $e->getMessage();
+            }
+        }
+
+        return $this->jms_json($data);
+    }
+
+    /**
+     * @Route("/{id}", methods={"DELETE"}, requirements={"id"="\d{1,10}"}, name="api_guest_delete")
+     */
+    public function delete($id)
+    {
+        $data = array();
+
+        $guest = $this->getDoctrine()->getRepository(Guest::class)->find($id);
+        
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($guest);
+            $em->flush();
+            $data["message"] = "Successfully deleted";
+        } catch(\Exception $e) {
+            $data["message"] = "Failed to delete";
+
+            //TODO cleaner error handling
+            $data["error"] = $e->getMessage();
+        }
+
+        return $this->jms_json($data);
     }
 }
