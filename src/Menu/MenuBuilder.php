9d87d4e8d0cecea3af79970196a05d3025d13eb9
Koen Loogman <koen@loogman.de>
2018-03-22 09:04:35 +0100

    added menu knp service

---
+++ src/Menu/MenuBuilder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+
+class MenuBuilder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     *
+     * Add any other dependency you need
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(array $options)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Home', array('route' => 'index'));
+        // ... add more children
+
+        return $menu;
+    }
+}
+
+?>
