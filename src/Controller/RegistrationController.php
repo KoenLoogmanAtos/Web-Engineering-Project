5983043f34b46fcc3053ac170b6f33d38a9e8fd7
Kader <aleyna.kader@atos.net>
2018-03-29 11:14:04 +0200

    added function "login"

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class RegistrationController extends Controller
 {
@@ -44,4 +45,18 @@ class RegistrationController extends Controller
             array('form' => $form->createView())
         );
     }
+
+    public function login (Request $request, AuthenticationUtils $authenticationUtils)
+{
+    // get the login error if there is one
+    $error = $authenticationUtils->getLastAuthenticationError();
+
+    // last username entered by the user
+    $lastUsername = $authenticationUtils->getLastUsername();
+
+    return $this->render('security/login.html.twig', array(
+        'last_username' => $lastUsername,
+        'error'         => $error,
+    ));
+}
 }
