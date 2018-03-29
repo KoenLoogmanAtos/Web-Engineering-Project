a7dab4c79bfcb17264a35fc1db1b528b94b1ab23
Koen Loogman <koen@loogman.de>
2018-03-29 14:15:13 +0200

    added editForm

--- src/Controller/RoomTypeController.php
+++ src/Controller/RoomTypeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\RoomType;
+use App\Form\RoomTypeType;
 
 class RoomTypeController extends Controller
 {
@@ -12,8 +14,17 @@ class RoomTypeController extends Controller
      */
     public function index()
     {
+        $roomTypes = $this->getDoctrine()->getRepository(RoomType::class)->findAll();
+
+        $roomType = new RoomType();
+        $editForm = $this->createForm(RoomTypeType::class, $roomType, array(
+            'action' => $this->generateUrl('api_room_type_edit', ["id" => 0]),
+            'method' => 'PUT',
+        ));
+
         return $this->render('room_type/index.html.twig', [
-            'controller_name' => 'RoomTypeController',
+            'roomTypes' => $roomTypes,
+            'editForm' => $editForm->createView(),
         ]);
     }
 }
