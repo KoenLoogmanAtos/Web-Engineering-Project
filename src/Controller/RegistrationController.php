4c3af495f0d87474cf5fa937ac230bcef430e246
Kader <aleyna.kader@atos.net>
2018-04-04 20:57:44 +0200

    ich hab versucht den pfad da irgendwie hinzubekommen

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -57,7 +57,7 @@ class RegistrationController extends Controller
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
         
-        return $this->render('security/login.html.twig', array(
+        return $this->render('base.html.twig', array(
             'last_username' => $lastUsername,
             'error'         => $error,
         ));
