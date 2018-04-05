de58a1084a0012275c831bfeff42e72b0d2e93e1
Koen Loogman <koen@loogman.de>
2018-04-05 11:09:45 +0200

    changed routes to be more simple

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,31 +13,24 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends Controller
 {
     /**
-     * @Route("/register", name="user_registration")
+     * @Route("/register", name="registration")
      */
     public function registerAction(Request $request, UserPasswordEncoderInterface $encoder)
     {
-        // 1) build the form
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
 
-        // 2) handle the submit (will only happen on POST)
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
-            // 3) Encode the password (you could also do this via Doctrine listener)
             $password = $encoder->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
 
-            // 4) save the User!
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
 
-            // ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
-
-            return $this->redirectToRoute('index');
+            return $this->redirectToRoute('login');
         }
 
         return $this->render('security/register.html.twig',
@@ -46,7 +39,7 @@ class SecurityController extends Controller
     }
 
     /**
-     * @Route("/login", name="user_login")
+     * @Route("/login", name="login")
      */
     public function login (Request $request, AuthenticationUtils $authenticationUtils)
     {
