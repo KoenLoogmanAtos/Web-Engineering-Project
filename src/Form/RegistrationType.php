d15a4a925d3fcde404021d7f3d7650d9b0db06bb
Koen Loogman <koen@loogman.de>
2018-04-05 13:48:36 +0200

    renamed form

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -10,8 +10,9 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class UserType extends AbstractType
+class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -21,8 +22,9 @@ class UserType extends AbstractType
         ->add('plainPassword', RepeatedType::class, array(
             'type' => PasswordType::class,
             'first_options'  => array('label' => 'Password'),
-            'second_options' => array('label' => 'Repeat Password'),
+            'second_options' => array('label' => 'Repeat Password')
         ))
+        ->add('submit', SubmitType::class)
         ;
     }
 
