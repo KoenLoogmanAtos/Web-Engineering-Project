b42212d324067682fa19170a19169e226268ec49
Aleyna Kader <aleyna.kader@atos.net>
2018-04-12 10:53:32 +0200

    ui templates

--- src/Controller/UIController.php
+++ src/Controller/UIController.php
@@ -5,15 +5,28 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * @Route("/ui/", name="ui")
+ */
 class UIController extends Controller
 {
     /**
-     * @Route("/u/i", name="u_i")
+     * @Route("index", name="_index")
      */
     public function index()
     {
         return $this->render('ui/index.html.twig', [
             'controller_name' => 'UIController',
+        ]);
+    }
+
+    /**
+     * @Route("rooms", name="_rooms")
+     */
+    public function rooms()
+    {
+        return $this->render('ui/rooms.html.twig', [
+            'message' => 'Welcome!!!',
         ]);
     }
 }
