bed4d9538b0c4b8c18d5e5b2da9199b815b6539f
Koen Loogman <koen@loogman.de>
2018-05-13 21:13:59 +0200

    dependency

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Booking;
 
 /**
  * @Route("/admin", name="admin")
